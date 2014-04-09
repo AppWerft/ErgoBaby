90c824adff1418983b5e9cccc95b6e57371d7224
AppWerft <rs@hamburger-appwerft.de>
2014-04-09 15:34:08 +0200

       

--- Resources/ui/shop/colorfilter.widget.js
+++ Resources/ui/shop/colorfilter.widget.js
@@ -1,4 +1,4 @@
-exports.create = function(color) {
+function getColor(color) {
 	var self = Ti.UI.createView({
 		left : 2,
 		right : 2,
@@ -7,7 +7,8 @@ exports.create = function(color) {
 		top : 0,
 		backgroundColor : color,
 		borderColor : 'gray',
-		borderWidth : 0.5
+		borderWidth : 0.5,
+		zIndex : 9999
 	});
 	self.addEventListener('click', function() {
 		if (self.height == 40)
@@ -16,5 +17,26 @@ exports.create = function(color) {
 			self.height = 40;
 	});
 	return self;
+}
 
+exports.create = function() {
+	var self = Ti.UI.createView({
+		height : '60dp',
+		bubbleParent : false,
+		top : '-60dp'
+	});
+	self.add(Ti.UI.createView({
+		backgroundColor : 'black',
+		opacity : 0.7
+	}));
+	self.container = Ti.UI.createView({
+		layout : 'horizontal',
+		horizontalWrap : false
+	});
+	self.add(self.container);
+	var colors = ['#990000', 'blue', '#00aa00', '#dddd77', 'gray', 'orange', 'black'];
+	for (var i = 0; i < colors.length; i++) {
+		self.container.add(getColor(colors[i]));
+	}
+	return self;
 };
--- Resources/ui/shop/productlist.window.js
+++ Resources/ui/shop/productlist.window.js
@@ -1,25 +1,19 @@
+var abextras = require('com.alcoapps.actionbarextras');
+
 exports.create = function(title) {
 	var self = require('vendor/window').create({
-		title : title
+		title : 'ErgoBaby'
 	});
-	var colorfilter = Ti.UI.createView({
-		top : 0,
-		backgroundColor : 'transparent',
-		layout : 'horizontal',
-		horizontalWrap : false,
-		height : '60dp'
-	});
-	var colors = ['red', 'blue', 'green', 'yellow', 'gray', 'orange', 'black'];
-	for (var i = 0; i < colors.length; i++) {
-		colorfilter.add(require('ui/colorfilter.widget').create(colors[i]));
-	}
+	self.filtervisible = false;
+	var colorfilter = require('ui/shop/colorfilter.widget').create();
+	var pricefilter = require('ui/shop/pricefilter.widget').create();
 	var cats = require('model/shop');
 	self.listview = Ti.UI.createListView({
 		sections : [Ti.UI.createListSection({
 			headerTitle : null,
 
 		})],
-		top : '40dp',
+		top : 0,
 		templates : {
 			'template' : require('ui/TEMPLATES').items
 		},
@@ -30,28 +24,103 @@ exports.create = function(title) {
 	for (var i = 0; i < cats.length; i++) {
 		if (cats[i].items) {
 			for (var j = 0; j < cats[i].items.length; j++) {
-				rows.push({
-					properties : {
-						accessoryType : Ti.UI.LIST_ACCESSORY_TYPE_DISCLOSURE,
-						itemId : cats[i].items[j].title
-					},
-					title : {
-						text : cats[i].items[j].title
-					},
-					color : {
-						text : cats[i].items[j].color
-					},
-					price : {
-						text : cats[i].items[j].price
-					},
-					image : {
-						image : cats[i].items[j].image
-					}
-				});
+				if (cats[i].items.title)
+					rows.push({
+						properties : {
+							accessoryType : Ti.UI.LIST_ACCESSORY_TYPE_DISCLOSURE,
+							itemId : cats[i].items[j].title
+						},
+						title : {
+							text : cats[i].items[j].title
+						},
+						color : {
+							text : 'Color: ' + cats[i].items[j].color || 'unknown'
+						},
+						price : {
+							text : 'Price: ' + parseFloat(cats[i].items[j].price).toFixed(2) + ' €'
+						},
+						image : {
+							image : cats[i].items[j].image
+						}
+					});
 			}
 		}
 	}
 	self.listview.getSections()[0].setItems(rows);
 	self.add(colorfilter);
+	self.add(pricefilter);
+	self.addEventListener('open', function() {
+		if (!Ti.Android)
+			return;
+		var activity = self.getActivity();
+		if (!activity.actionBar)
+			return;
+		abextras.setExtras({
+			title : 'ErgoBaby',
+			subtitle : title
+		});
+		activity.onCreateOptionsMenu = function(e) {
+			e.menu.add({
+				title : 'Basket',
+				showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM,
+				icon : '/assets/basket.png'
+			}).addEventListener("click", function() {
+				Ti.Android && Ti.UI.createNotification({
+					message : 'Your basket is empty.'
+				}).show();
+			});
+			e.menu.add({
+				title : 'Filter',
+				showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM,
+				icon : '/assets/filter.png'
+			}).addEventListener("click", function() {
+				if (self.filtervisible) {
+					colorfilter.animate({
+						top : '-60dp'
+					});
+					pricefilter.animate({
+						bottom : '-60dp'
+					});
+					self.listview.animate({
+						top : 0,
+						bottom : 0
+					});
+					self.filtervisible = false;
+				} else {
+					colorfilter.animate({
+						top : 0
+					});
+					self.listview.animate({
+						top : '40dp',
+						bottom : '80dp'
+					});
+					pricefilter.animate({
+						bottom : 0
+					});
+					self.filtervisible = true;
+				}
+			});
+			/*			e.menu.add({
+			 showAsAction : Ti.Android.SHOW_AS_ACTION_NEVER,
+			 }).addEventListener("click", function() {
+
+			 });*/
+		};
+	});
+	self.addEventListener('touchmove', function() {
+		if (self.filtervisible) {
+			colorfilter.animate({
+				top : '-60dp'
+			});
+			pricefilter.animate({
+				bottom : '-60dp'
+			});
+			self.listview.animate({
+				top : 0,
+				bottom : 0
+			});
+			self.filtervisible = false;
+		}
+	});
 	return self;
 };
